3d9950a76e2b9fbadd00244f7b44d1435615ffdb
Alice Zeidler <avzeidler@gmail.com>
2024-09-26 20:41:59 -0300

    adjusting a lot of things

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,40 @@
-const popup1 = document.getElementById('popup1');
-const popup2 = document.getElementById('popup2');
-const closeButton1 = document.querySelector('#popup1 .close-button');
-const closeButton2 = document.querySelector('#popup2 .close-button');
-
-
-function openPopup(popupId) {
-    document.body.classList.add('no-scroll')
-    document.getElementById(popupId).style.display = 'flex'
-}
-
-
-function closePopup(popupId) {
-    document.body.classList.remove('no-scroll')
-    document.getElementById(popupId).style.display = 'none'
-}
-
-
-closeButton1.onclick = function () {
-    closePopup('popup1')
-}
-
-closeButton2.onclick = function () {
-    closePopup('popup2')
-}
-
-
 window.onload = function () {
-    setTimeout(() => {
-        openPopup('popup1')
-    }, 2000)
+    const countdownElement = document.getElementById('countdown');
+    const targetDate = new Date('2024-09-12T00:00:00');
+    const popup = document.getElementById('popup');
+    const btnAceito = document.getElementById('btnAceito');
+    const btnFechar = document.getElementById('btnFechar');
+    const btnClose = document.getElementById('btnClose');
 
-    setTimeout(() => {
-        openPopup('popup2')
-    }, 5000)
-}
+    function updateCountdown() {
+        const now = new Date();
+        const timeDifference = targetDate - now;
 
-function closeWindow() {
-    window.close();
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        if (timeDifference < 0) {
+            clearInterval(countdownInterval);
+            countdownElement.innerHTML = "Acabou o tempo...";
+        }
+    }
+
+    const countdownInterval = setInterval(updateCountdown, 1000);
+
+
+    btnAceito.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+    btnFechar.addEventListener('click', () => {
+        window.close();
+    });
+
+    btnClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
 }
